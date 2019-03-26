0791b07d0663fe14b1e0b1aa3af1cb3cfdc57b52
Alberto Morales <cmtmorales24@gmail.com>
2019-03-26 14:58:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,6 +68,7 @@ self.pets={}
 end
 
 def list_pets
+  binding.pry
   # for each key, get the count of items in Array
 "I have #{self.pets[:fish].size} fish, #{self.pets[:dogs].size} dog(s), and #{self.pets[:cats].size} cat(s)."
 end 
